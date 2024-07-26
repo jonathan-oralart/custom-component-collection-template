2d10b9c8a47bb9d6dbe408590584cfc4c11b8033
Jonathan <jonathan@oralart.co.nz>
2024-07-26 14:31:28 +1200

    changes

--- src/teethSelector.tsx
+++ src/teethSelector.tsx
@@ -1,13 +1,5 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback, useMemo } from 'react'
 import { teethData } from './teethData'
-
-export type ToothProps = {
-  toothNum: number
-  textTransform: string
-  pathTransform: string
-  d: string
-  arch: 'upper' | 'lower'
-}
 
 type TeethSelectionProps = {
   onChange: (teeth: string) => void
@@ -15,34 +7,31 @@ type TeethSelectionProps = {
   setValue: (teeth: string) => void
 }
 
-function Tooth(
-  props: {
-    toothNum: number
-    textTransform: string
-    pathTransform: string
-    d: string
-    isDragging: boolean
-    onMouseEnter: () => void
-    onMouseDown: () => void
-  } & TeethSelectionProps
-) {
-  const selected = props.value
-    .split(/[\s,]+/)
-    .map((x) => parseInt(x))
-    .includes(props.toothNum)
-
+function Tooth({
+  toothNum,
+  textTransform,
+  pathTransform,
+  d,
+  selected,
+  onMouseDown,
+  onMouseEnter
+}: {
+  toothNum: number
+  textTransform: string
+  pathTransform: string
+  d: string
+  selected: boolean
+  onMouseDown: (toothNum: number) => void
+  onMouseEnter: (toothNum: number) => void
+}) {
   return (
     <g
       style={{ fill: selected ? '#3a88d7' : '#e8e8e8', cursor: 'pointer' }}
-      id={`tooth-${props.toothNum}`}
-      onMouseDown={props.onMouseDown}
-      onMouseEnter={props.onMouseEnter}
+      id={`tooth-${toothNum}`}
+      onMouseDown={() => onMouseDown(toothNum)}
+      onMouseEnter={() => onMouseEnter(toothNum)}
     >
-      <path
-        className="tooth_path"
-        d={props.d}
-        transform={props.pathTransform}
-      ></path>
+      <path className="tooth_path" d={d} transform={pathTransform} />
       <text
         style={{
           fontSize: '2.3em',
@@ -51,15 +40,19 @@ function Tooth(
           userSelect: 'none'
         }}
         textAnchor="middle"
-        transform={props.textTransform}
+        transform={textTransform}
       >
-        {props.toothNum}
+        {toothNum}
       </text>
     </g>
   )
 }
 
-function TeethSelectionBase(props: TeethSelectionProps) {
+function TeethSelectionBase({
+  onChange,
+  value,
+  setValue
+}: TeethSelectionProps) {
   const [isDragging, setIsDragging] = useState(false)
 
   useEffect(() => {
@@ -68,86 +61,82 @@ function TeethSelectionBase(props: TeethSelectionProps) {
     return () => window.removeEventListener('mouseup', handleMouseUp)
   }, [])
 
-  const toggleSelection = useCallback(
+  const selectedTeeth = useMemo(
+    () =>
+      new Set(
+        value
+          .split(/[\s,]+/)
+          .map((x) => parseInt(x))
+          .filter(Boolean)
+      ),
+    [value]
+  )
+
+  const toggleTooth = useCallback(
     (toothNum: number) => {
-      const selectedTeeth = props.value.split(/[\s,]+/).map((x) => parseInt(x))
-      let newSelection: number[]
-
-      if (selectedTeeth.includes(toothNum)) {
-        newSelection = selectedTeeth.filter((t) => t !== toothNum)
+      const newSelection = new Set(selectedTeeth)
+      if (newSelection.has(toothNum)) {
+        newSelection.delete(toothNum)
       } else {
-        newSelection = [...selectedTeeth, toothNum]
+        newSelection.add(toothNum)
       }
-
-      const joinedValues = newSelection.join(', ')
-      props.setValue(joinedValues)
-      props.onChange(joinedValues)
+      const newValue = Array.from(newSelection).join(', ')
+      setValue(newValue)
+      onChange(newValue)
     },
-    [props]
+    [selectedTeeth, setValue, onChange]
   )
 
   const handleMouseDown = useCallback(
     (toothNum: number) => {
       setIsDragging(true)
-      toggleSelection(toothNum)
+      toggleTooth(toothNum)
     },
-    [toggleSelection]
+    [toggleTooth]
   )
 
   const handleMouseEnter = useCallback(
     (toothNum: number) => {
       if (isDragging) {
-        toggleSelection(toothNum)
+        toggleTooth(toothNum)
       }
     },
-    [isDragging, toggleSelection]
+    [isDragging, toggleTooth]
+  )
+
+  const renderTeeth = useCallback(
+    (arch: 'upper' | 'lower') =>
+      teethData
+        .filter((tooth) => tooth.arch === arch)
+        .map((tooth) => (
+          <Tooth
+            key={tooth.toothNum}
+            {...tooth}
+            selected={selectedTeeth.has(tooth.toothNum)}
+            onMouseDown={handleMouseDown}
+            onMouseEnter={handleMouseEnter}
+          />
+        )),
+    [selectedTeeth, handleMouseDown, handleMouseEnter]
   )
 
   return (
-    <>
-      <div className="tooth_chart_container">
-        <div className="tooth_chart">
-          <span className="tooth_arch">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              id="Layer_1"
-              viewBox="0 0 1305.645 223.836"
-              style={{ maxHeight: '70px', maxWidth: '100%' }}
-            >
-              <title>Teeth Chart</title>
-              <g className="upper_arch arch_wrapper">
-                {teethData
-                  .filter((tooth) => tooth.arch === 'upper')
-                  .map((tooth) => (
-                    <Tooth
-                      key={tooth.toothNum}
-                      {...props}
-                      {...tooth}
-                      isDragging={isDragging}
-                      onMouseDown={() => handleMouseDown(tooth.toothNum)}
-                      onMouseEnter={() => handleMouseEnter(tooth.toothNum)}
-                    />
-                  ))}
-              </g>
-              <g className="lower_arch arch_wrapper">
-                {teethData
-                  .filter((tooth) => tooth.arch === 'lower')
-                  .map((tooth) => (
-                    <Tooth
-                      key={tooth.toothNum}
-                      {...props}
-                      {...tooth}
-                      isDragging={isDragging}
-                      onMouseDown={() => handleMouseDown(tooth.toothNum)}
-                      onMouseEnter={() => handleMouseEnter(tooth.toothNum)}
-                    />
-                  ))}
-              </g>
-            </svg>
-          </span>
-        </div>
+    <div className="tooth_chart_container">
+      <div className="tooth_chart">
+        <span className="tooth_arch">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            id="Layer_1"
+            viewBox="0 0 1305.645 223.836"
+            style={{ maxHeight: '70px', maxWidth: '100%' }}
+          >
+            <title>Teeth Chart</title>
+            <g className="upper_arch arch_wrapper">{renderTeeth('upper')}</g>
+            <g className="lower_arch arch_wrapper">{renderTeeth('lower')}</g>
+          </svg>
+        </span>
       </div>
-    </>
+    </div>
   )
 }
 
